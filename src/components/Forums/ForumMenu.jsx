48e4064c15a1448a3ad6057bcce3e2b11b77edcb
Fred Robins <fredrobins619@outlook.com>
2025-08-04 19:09:01 +0000

    Upgrade existing Forums components

--- src/components/Forums/ForumMenu.jsx
+++ src/components/Forums/ForumMenu.jsx
@@ -1,3 +1,6 @@
+import { Link } from 'react-router-dom'
+import MentorSvg from '../../assets/images/forumIcons/MentorProfile.svg'
+
 function ForumMenu({ title, children }) {
     return (
         <div className="border-[0.25px] border-transparent rounded-lg overflow-hidden">
@@ -17,10 +20,12 @@ export default ForumMenu
 
 export function ForumMenuKids({ title, number }) {
     return (
-        <div className="flex justify-between px-4 py-3 text-xs hover:bg-[#E6F0F2] font-GeneralSans-Medium">
-            <p className="text-[#47484B]">{title}</p>
-            <span className="text-[#797B80]">{number}</span>
-        </div>
+        <Link to={`/forums/topic/${title}`}>
+            <div className="flex justify-between px-4 py-3 text-xs hover:bg-[#E6F0F2] font-GeneralSans-Medium">
+                <p className="text-[#47484B]">{title}</p>
+                <span className="text-[#797B80]">{number}</span>
+            </div>
+        </Link>
     )
 }
 
@@ -28,7 +33,7 @@ export function ForumMenuPpl({ name, rank }) {
     return (
         <div className="flex justify-between px-4 py-3 text-xs font-GeneralSans-Medium ">
             <div className="flex gap-2">
-                <img src="" alt="$$" />
+                <img src={MentorSvg} alt="$$" className="max-h-[20.25px] max-w-[20.25px] " />
                 <p className="text-[#47484B]">{name}</p>
             </div>
             <span className="text-[#797B80]">{rank}</span>
--- src/components/Forums/ForumTile.jsx
+++ src/components/Forums/ForumTile.jsx
@@ -1,37 +1,58 @@
-function ForumTile({ studentName, channelName, time, topic, topicDetail}) {
+import ThreeDotsSvg from '../../assets/images/forumIcons/DotsThreeVertical.svg'
+import ShareSvg from '../../assets/images/forumIcons/ShareNetwork.svg'
+import EyeSvg from '../../assets/images/forumIcons/Eye.svg'
+import ChatSvg from '../../assets/images/forumIcons/Chat.svg'
+import HeartSvg from '../../assets/images/forumIcons/Heart.svg'
+import { Link } from 'react-router-dom'
+
+function ForumTile({ studentName, channelName, time, topic, topicDetail,heartsNo, repliesNo, views }) {
     return (
         <div className="p-3 border-[0.25px] border-[#CACDD5] rounded-lg text-xs">
             <div className="flex justify-between max-md:flex-col">
                 <div className="flex items-start md:items-center gap-[11px] max-md:flex-col">
                     <div className="flex items-center gap-1">
                         <div className="w-6 h-6 rounded-full bg-[#D9D9D9]"></div>
-                        <p className="font-GeneralSans-Medium">{studentName} in <span className="text-[#025663]">{channelName}</span></p>
+                        <p className="font-GeneralSans-Medium">{studentName} in <Link to={`/forums/topic/${channelName}`}><span className="text-[#025663]">{channelName}</span></Link> </p>
                     </div>
-                    <div className="flex items-center gap-1">
+                    <div className="flex items-center gap-1 max-md:ml-6">
                         <div className="w-[3px] h-[3px] rounded-full bg-[#989AA0]"></div>
-                        <span className="text-[#989AA0]">{time}</span>
+                        <span className="text-[#989AA0] text-[10px]">{time}</span>
                     </div>
                 </div>
 
                 <div className="flex items-center gap-2 max-md:mt-3">
                     <button className="bg-blue-500 text-[#FAFAFB] rounded-lg py-1 px-4 h-8 font-GeneralSans-Medium text-sm">Join</button>
                     {/* Hamburger Icon */}
-                    <img src="" alt=":" className="w-5 h-5"/>
+                    <img src={ThreeDotsSvg} alt=":" className="w-5 h-5 cursor-pointer"/>
                 </div>
             </div>
 
-            <div className="mt-5 mb-8">
+            <div className="mt-5 mb-8 text-[#353535]">
                 <h1 className="text-base font-GeneralSans-Semibold mb-[14px]">{topic}</h1>
-                <p className="text-[#353535] text-sm font-GeneralSans-Medium">{topicDetail}</p>
+                <p className="text-sm font-GeneralSans-Medium">{topicDetail}</p>
             </div>
 
-            <div>
-                <div>
+            <div className='flex flex-row justify-between text-sm font-GeneralSans-Medium text-[#797B80]'>
+                <div className='flex gap-6'>
                     {/* 3 Img's */}
+                    <div className='flex gap-1 cursor-pointer'>
+                        <img src={HeartSvg} alt="heart" className='' />
+                        <p>{heartsNo}</p>
+                    </div>
+                    <Link to={"/forums/response"} >
+                        <div className='flex gap-1 cursor-pointer'>
+                            <img src={ChatSvg} alt="chat" className='' />
+                            <p>{repliesNo}</p>
+                        </div>
+                    </Link>
+                    <div className='flex gap-1 cursor-pointer'>
+                        <img src={EyeSvg} alt="eye" className='' />
+                        <p>{views}</p>
+                    </div>
                 </div>
 
                 <div>
-                    {/* Share icon */}
+                    <img src={ShareSvg} alt="share" />
                 </div>
             </div>
         </div>
